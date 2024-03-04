c8d598dda949c6cf56e53059b482e5dd97a7956d
prakashs24 <prakashs24dev@gmail.com>
2024-03-04 13:03:17 +0530

    addfirst

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import UseState from './Hooks/UseState';
+import { useState } from 'react';
 
 function App() {
-  return (
-  <UseState />
-  );
+   const [details,setDetails]=useState({counter:0,name:""});
+   function increaseCounter(){
+    setDetails((prev)=>({
+      ...prev,
+      counter: prev.counter + 1,
+    }));
+   }
+   console.log(details);
+   return(
+    <div>
+    <input type='text' onChange={(e)=>e.target.value} />
+    <h1>  
+      {details.name} has clicked {details.counter} times!!
+    </h1>  
+    <button onClick={increaseCounter}>Increase</button>
+    </div>
+   )
 }
-
 export default App;
